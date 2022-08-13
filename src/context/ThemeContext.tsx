8cdaeb6c33171f7deccfa728420c19228ab125f8
TheKarls <carlobussi.dev@gmail.com>
2022-08-13 15:00:46 +0200

    Small fix

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,16 +1,16 @@
 import React, { createContext, useState } from 'react'
 
 export const ThemeContext = createContext<any>({
-    theme: 'dark',
-    undefined,
+  theme: 'dark',
+  undefined
 })
 
 export const ThemeProvider: React.FC<{}> = ({ children }) => {
-    const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState('dark')
 
-    return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
 }
--- src/hooks/useGamesProvider.tsx
+++ src/hooks/useGamesProvider.tsx
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 export const getHintList = async (typing: string): Promise<[]> => {
   const response = await axios({
-    url: 'https://gh2kfsa1hk.execute-api.us-west-2.amazonaws.com/production/v4/games',
+    url: 'https://gh2kfsa1hk.execute-api.us-west-2.amazonaws.com/production/v4/search',
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'x-api-key': process.env.NEXT_PUBLIC_API_KEY || ''
     },
-    data: `fields name; limit 5; search "${typing}";`
+    data: `fields name,id; limit 20; search "${typing}";`
   })
   return response.data
 }
@@ -43,6 +43,7 @@ export const getGameByName = async (name: string): Promise<[]> => {
     },
     data: `fields image_id; where game = ${gameData.data[0].id};`
   })
+  console.log(genreList.data)
 
   gameData.data[0].genres = genreList.data.map((genre: any) => genre.name)
   gameData.data[0].cover = `https://images.igdb.com/igdb/image/upload/t_cover_med_2x/${gameCover.data[0].image_id}.jpg`
